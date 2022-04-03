119f49fd4a70499c348a86a45359452954388d3e
rmaniego <rod.maniego23@gmail.com>
2022-04-03 12:40:37 +0800

    Add insert method

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 setuptools.setup(
     name = 'namari',
     packages = ["namari"],
-    version = '1.1.4',
+    version = '1.1.5',
     license='MIT',
     description = 'Many-to-one keys-value pair relationship Python object manager.',
     long_description=long_description,
